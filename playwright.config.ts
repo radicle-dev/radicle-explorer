d21434d25697e5f476d645ffb17e59b365598f14
Rūdolfs Ošiņš <rudolfs@osins.org>
2026-02-26 14:36:45 -0600

    Remove unused test projects
    We only run tests on chromium on CI.

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,18 +29,6 @@ const config: PlaywrightTestConfig = {
       },
     },
     {
-      name: "firefox",
-      use: {
-        ...devices["Desktop Firefox"],
-      },
-    },
-    {
-      name: "webkit",
-      use: {
-        ...devices["Desktop Safari"],
-      },
-    },
-    {
       name: "visual-desktop",
       timeout: 60_000,
       expect: {
